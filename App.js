420f38e6b5161d429ace89210017859ecd69f940
nhzq-1010 <18802121321@163.com>
2024-04-02 13:50:00 -0400

    first commit

---
+++ App.js
@@ -0,0 +1,19 @@
+import express from 'express';
+import Lab5 from "./Lab5.js";
+import Hello from "./Hello.js"
+import cors from "cors";
+import CourseRoutes from "./Kanbas/courses/routes.js";
+import ModuleRoutes from "./Kanbas/modules/routes.js";
+
+
+const app = express()
+app.use(cors());
+app.use(express.json());
+CourseRoutes(app);
+ModuleRoutes(app);
+Lab5(app);
+Hello(app);
+
+
+
+app.listen(process.env.PORT || 4000)
